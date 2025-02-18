61139a8a7138c05329dfaaa074da467df18a5df0
Pelinsu-Sahin <sahinpelinsu5@gmail.com>
2025-02-18 12:15:31 +0300

    list staff with api consuming

--- hotel_reservation/ApiConsume/HotelProject.WebApi/Program.cs
+++ hotel_reservation/ApiConsume/HotelProject.WebApi/Program.cs
@@ -32,7 +32,6 @@ builder.Services.AddCors(opt =>
     });
 });
 
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
