fa7466838fa22d88d551a6810eec3976c4fe735d
Pelinsu-Sahin <sahinpelinsu5@gmail.com>
2025-02-18 19:16:06 +0300

    adding new staff is handled

--- hotel_reservation/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ hotel_reservation/Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,4 +1,5 @@
-﻿using HotelProject.WebUI.Models.Staff;
+﻿using System.Text;
+using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -24,6 +25,25 @@ namespace HotelProject.WebUI.Controllers
                 return View(values);
             }
             return View();
+        }
+        [HttpGet]
+        public IActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task <IActionResult> AddStaff(AddStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5045/api/Staff", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
