d34397b6c1f34097180bc3bae40da02e99279a6b
Pelinsu-Sahin <sahinpelinsu5@gmail.com>
2025-03-09 23:23:21 +0300

    dto classes are started to create(room dto), automapper libraries are added

--- hotel_reservation/ApiConsume/HotelProject.WebApi/Program.cs
+++ hotel_reservation/ApiConsume/HotelProject.WebApi/Program.cs
@@ -3,6 +3,8 @@ using HotelProject.BusinessLayer.Concrete;
 using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.WebApi.Mapping;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +25,8 @@ builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
 
 builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
 builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
 
 builder.Services.AddCors(opt =>
 {
